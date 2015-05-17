e0580224fd1a34fbf55959f918714d031940e60a
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 13:38:26 -0400

    initial pipeline start

---
+++ ci/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CF_CLI_VERSION=$(cat cf-cli-release/version)
+
+
+url="https://cli.run.pivotal.io/stable?release=linux64-binary&version=${CF_CLI_VERSION}&source=docker-cf"
+curl -L -o packaging/cf-${CF_CLI_VERSION}-linux-x86_64.tgz ${url}
+
+tar xzf packaging/cf-${CF_CLI_VERSION}-linux-x86_64.tgz -C bin/
