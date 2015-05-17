0b187ef936fd205a88c80a5ab6c1a13b84761782
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 13:43:12 -0400

    install curl

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,7 @@
 
 CF_CLI_VERSION=$(cat cf-cli-release/version)
 
+apt-get install curl
 
 url="https://cli.run.pivotal.io/stable?release=linux64-binary&version=${CF_CLI_VERSION}&source=docker-cf"
 curl -L -o packaging/cf-${CF_CLI_VERSION}-linux-x86_64.tgz ${url}
