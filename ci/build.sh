660d1e821feec8633dd898badddcf9ce98234f19
Long Nguyen <long.nguyen11288@gmail.com>
2015-05-17 14:19:08 -0400

    create bin

--- ci/build.sh
+++ ci/build.sh
@@ -5,4 +5,6 @@ CF_CLI_VERSION=$(cat cf-cli-release/version)
 url="https://cli.run.pivotal.io/stable?release=linux64-binary&version=${CF_CLI_VERSION}&source=docker-cf"
 curl -L -o cf-${CF_CLI_VERSION}-linux-x86_64.tgz ${url}
 
+mkdir -p bin
+
 tar xzf cf-${CF_CLI_VERSION}-linux-x86_64.tgz -C bin/
